a24b3b97195a181f73b61aa5beee254640f8794d
alterok <krishan-sharma@live.com>
2024-06-21 00:47:12 +0530

    added flip, evaluate in BooleanExt.kt

--- lib-kotlin-essential-extensions/src/main/java/com/alterok/kotlin_essential_extensions/BooleanExt.kt
+++ lib-kotlin-essential-extensions/src/main/java/com/alterok/kotlin_essential_extensions/BooleanExt.kt
@@ -9,3 +9,19 @@ inline fun Boolean.ifFalse(block: () -> Unit): Boolean {
     if (!this) block()
     return this
 }
+
+fun <R> Boolean.evaluate(onTrue: R, onFalse: R): R {
+    return if (this)
+        onTrue
+    else
+        onFalse
+}
+
+inline fun <R> Boolean.evaluate(onTrue: () -> R, onFalse: () -> R): R {
+    return if (this)
+        onTrue()
+    else
+        onFalse()
+}
+
+fun Boolean.flip() = !this
